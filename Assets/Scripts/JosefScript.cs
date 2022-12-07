30bd3ef44f88499f438b8f05b78adc819f4de2ff
Jarh0 <52079747+JaroslavGit@users.noreply.github.com>
2022-12-07 14:59:04 +0100

    Escape 1.0

---
+++ Assets/Scripts/JosefScript.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class JosefScript : MonoBehaviour
+{
+    [SerializeField]
+    public float speed = 3.0f;
+    public float jump = 1.0f;
+    private float moveH;
+    private float moveV;
+
+    public bool isJumping;
+    public Animator anim;
+    public SpriteRenderer srender;
+    public Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();  
+        srender = gameObject.GetComponent<SpriteRenderer>();
+        rig = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Attack();
+     
+    }
+
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+          {
+            anim.SetBool("isAttacking", true);
+        }
+        else {
+            anim.SetBool("isAttacking", false);
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (!isJumping)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+     moveV = Input.GetAxisRaw("Vertical");
+     rig.AddForce(new Vector2(0f, moveV)*jump, ForceMode2D.Impulse);
+    }
+
+    private void Move()
+    {
+        moveH = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(moveH, 0f, 0f) * speed * Time.deltaTime;
+        if (moveH > 0f)
+        {
+            anim.SetBool("isRunning", true);
+            srender.flipX = false;
+        } else
+        
+        if (moveH < 0f)
+            {
+             anim.SetBool("isRunning", true);
+             srender.flipX = true;
+            }
+        
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isJumping = false;
+        }    
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isJumping = true; ;
+        }
+
+    }
+}
